4b305a373c4c2b67353724458d00ce8fbaa4044c
SyedHamzaAsim <hamza.asim090@gmail.com>
2022-05-31 23:59:18 +0000

    Eq for Arrays

---
+++ eqArrays.js
@@ -0,0 +1,22 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (actualArray, ExpectedArray) {
+  let answer = true;
+  for (let i = 0; i < actualArray.length; i++) {
+    if (actualArray[i] !== ExpectedArray[i]) {
+      answer = false;
+    }
+  }
+  return answer;
+}
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([4, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, "hello"], [1, 2, "hello"]), true);
