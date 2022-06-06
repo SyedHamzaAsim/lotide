7827d4c0a28d99b48da0ae82b591ccccd85aa754
SyedHamzaAsim <hamza.asim090@gmail.com>
2022-06-06 17:16:37 +0000

    refactoring lotide functions

--- head.js
+++ head.js
@@ -1,16 +1,9 @@
+const assertEqual = require('./assertEqual');
+
+
 function head(arrayOfActual)
 {
   return arrayOfActual[0];
 }
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+module.exports = head;
--- tail.js
+++ tail.js
@@ -1,21 +1,9 @@
+const assertEqual = require('./assertEqual');
+
 const tail = function(arrayOfActual) {
   let tail = [];
   tail = arrayOfActual.slice(1);
   return tail;
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
